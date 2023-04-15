5d61436b062769d6c03c72270cfcd3169e2a6832
puthukkottai <sundar160699@gmail.com>
2023-04-15 20:06:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ComponentD from "./Component/ComponentD";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="heading">
+        {" "}
+        <h1>simple-curd-operation</h1>
+      </div>
+
+      <br />
+      <ComponentD />
+    </div>
+  );
+}
+
+export default App;
